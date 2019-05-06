cc3ecbe9c895bdbf635976e24407becc6edaafee
Jongwoo Han <han980817@gmail.com>
2019-05-06 23:08:37 +0900

    Added Template : home

--- festivalproject/urls.py
+++ festivalproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import page.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', page.views.home, name="home"),
 ]
