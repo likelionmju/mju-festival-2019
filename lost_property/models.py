62612a56a918d77219d5f5a05c1666c2b36f272a
Jeonghun <bjh5230@gmail.com>
2019-05-10 13:56:37 +0900

    Modified Lost Model

--- lost_property/models.py
+++ lost_property/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Lost(models.Model):
+    title = models.CharField(max_length=50) 
+    author = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('publish')
+    image = models.ImageField()
+    content = models.TextField() 
+    password = models.CharField(max_length=20)
+    found = models.BooleanField(default=False)
