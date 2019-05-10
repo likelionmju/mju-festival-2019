dbb8b46611903e13f91cb1ae52c1a2621c6b2a70
Jeonghun <bjh5230@gmail.com>
2019-05-11 03:28:16 +0900

    Added mjworld.html

--- mjworld/views.py
+++ mjworld/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Mjworld
 
 # Create your views here.
+def mjworld(request):
+    mjworlds = Mjworld.objects.all()
+    return render(request, 'mjworld.html', {'mjworlds':mjworlds})
