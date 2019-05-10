8d6002d26db6ee1d4de06a1bdf967afdb1af6406
Jeonghun <bjh5230@gmail.com>
2019-05-11 02:32:56 +0900

    Added Mjworld Model

--- mjworld/admin.py
+++ mjworld/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Mjworld
 
 # Register your models here.
+admin.site.register(Mjworld)
--- mjworld/models.py
+++ mjworld/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Mjworld(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True)
+    address = models.CharField(max_length=100)
+    kind = models.CharField(max_length=50)
+    tel = models.CharField(max_length=20)
+    price = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.title
