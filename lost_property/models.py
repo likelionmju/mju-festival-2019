ee3c4e4fca5be9b07553a88b23a51049f4cbfd64
Jeonghun <bjh5230@gmail.com>
2019-05-10 14:49:10 +0900

    Modified Lost Model : allow none-images

--- lost_property/models.py
+++ lost_property/models.py
@@ -5,7 +5,10 @@ class Lost(models.Model):
     title = models.CharField(max_length=50) 
     author = models.CharField(max_length=20)
     pub_date = models.DateTimeField('publish')
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/', blank=True)
     content = models.TextField() 
     password = models.CharField(max_length=20)
     found = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
