c7a79cd0b6506877e6ce3977dae63e8b44b19f22
Jeonghun <bjh5230@gmail.com>
2019-05-11 03:45:20 +0900

    Added Template : lost, lost_new, lost_detail

--- lost_property/urls.py
+++ lost_property/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='lost_home'),
+    path('new/', views.new, name='lost_new'),
+    path('detail/', views.detail, name='detail_detail'),
 ]
 
 
--- lost_property/views.py
+++ lost_property/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'lost.html')
+
+def new(request):
+    return render(request, 'lost_new.html')
+
+def detail(request):
+    return render(request, 'lost_detail.html')
