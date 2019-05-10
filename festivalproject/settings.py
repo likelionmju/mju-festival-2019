f914b48a622942fc8236550231b228b52a36af24
Jeonghun <bjh5230@gmail.com>
2019-05-10 14:40:08 +0900

    Modified Settings : static, media

--- festivalproject/settings.py
+++ festivalproject/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'page' 'static'),
+    os.path.join(BASE_DIR, 'lost_property' 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- festivalproject/urls.py
+++ festivalproject/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 import page.views
 
+# media
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', page.views.home, name="home"),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
