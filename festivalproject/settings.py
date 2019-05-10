f10f702457183cfb4aef526edecb744f8b7e6a0c
Jeonghun <bjh5230@gmail.com>
2019-05-10 13:31:04 +0900

    Added App : lost_property

--- festivalproject/settings.py
+++ festivalproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'page.apps.PageConfig',
+    'lost_property.apps.LostPropertyConfig',
 ]
 
 MIDDLEWARE = [
