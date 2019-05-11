c08f343ada350ab242f5305e1644c0ba116d4c9e
ChoHyoungSeo <francesco.johs@gmail.com>
2019-05-11 23:49:18 +0900

    realize board_detail

--- board/views.py
+++ board/views.py
@@ -12,6 +12,6 @@ def new(request):
     return render(request, "board_new.html")
 
 #상세보기(detailed view)
-def detail(request):
+def detail(request, board_id):
     board_detail = get_object_or_404(Board, pk=board_id)
     return render(request, "board_detail.html", {'board_detail':board_detail})
