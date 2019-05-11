5eb3058774ec6168283ebb59387cd093c944b1d8
ChoHyoungSeo <francesco.johs@gmail.com>
2019-05-11 17:06:39 +0900

    initial board views

--- board/views.py
+++ board/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "post.html")
 
 #글작성 (new)
 def post_new(request):
     return render(request, "post_new.html")
 
 #상세보기(detailed view)
-def post_detail(request, post_id):
-    return render(request, "post.html")
+def post_detail(request):
+    return render(request, "post_detail.html")
