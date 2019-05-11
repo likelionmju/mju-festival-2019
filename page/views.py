3cdf213fff0f6bad7c393704ef17817ecc6813f0
Jeonghun <bjh5230@gmail.com>
2019-05-11 15:50:20 +0900

    Added Templates : info_page

--- page/views.py
+++ page/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def booth(request):
+    return render(request, "booth.html")
+
+def foodtruck(request):
+    return render(request, "foodtruck.html")
+
+def lineup(request):
+    return render(request, "lineup.html")
+
+def bus(request):
+    return render(request, "bus.html")
