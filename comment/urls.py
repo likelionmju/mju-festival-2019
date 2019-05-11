dda480f8347322618279ab9e20d77b6a371e8248
bbanderson <tombyun@naver.com>
2019-05-11 22:29:46 +0900

    Added lost_new button

--- comment/urls.py
+++ comment/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('board/new', views.board_new, name='board_new'),
     path('board/delete', views.board_delete, name='board_delete'),
-    path('lost/new', views.lost_new, name='lost_new'),
+    # path('lost/new', views.lost_new, name='lost_new'),
     path('lost/delete', views.lost_delete, name='lost_delete'),
 ]
