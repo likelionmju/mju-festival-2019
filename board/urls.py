bde1a75ef65b266b6b6adc5e600538f9e822f177
ChoHyoungSeo <francesco.johs@gmail.com>
2019-05-11 19:01:36 +0900

    modify post

--- board/urls.py
+++ board/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='post'),
     path('post_new/', views.post_new, name='post_new'),
-    path('post_detail/<int:post_id>', views.post_detail, name='post'),
+    path('post_detail/<int:post_id>', views.post_detail, name='post_detail'),
 ]
-
-
