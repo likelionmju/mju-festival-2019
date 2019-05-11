79d84fa22197803d57614fe0333c92ed6abec230
hwangknockjoo <hwangknock@gmail.com>
2019-05-11 16:52:06 +0900

    board

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='post'),
+    path('post_new/', views.post_new, name='post_new'),
+    path('post_detail/<int:post_id>', views.post_detail, name='post'),
 ]
 
 
