41b5bf46ab50dd3699eb5d95a20be015d275bb30
Jeonghun <bjh5230@gmail.com>
2019-05-12 22:53:07 +0900

    Modified Template : booth

--- page/urls.py
+++ page/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('foodtruck/', views.foodtruck, name='foodtruck'),
     path('lineup/', views.lineup, name='lineup'),
     path('bus/', views.bus, name='bus'),
-    path('individual/', views.individual, name="individual"),
-    path('organization/', views.organization, name="organization"),
+    path('booth/individual/', views.individual, name="individual"),
+    path('booth/organization/', views.organization, name="organization"),
 ]
