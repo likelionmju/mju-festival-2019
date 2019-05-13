8227cfe710fd1a488ccae77c265e98d2539eea3e
Jeonghun <bjh5230@gmail.com>
2019-05-13 11:43:50 +0900

    Modified Template : add board comment

--- comment/views.py
+++ comment/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import LostComment
+from .models import LostComment, BoardComment
 from lost_property.models import Lost
+from board.models import Board
 
 # Create your views here.
-def board_new(request):
-    return redirect('board_detail')
+def board_new(request, board_id):
+    board = get_object_or_404(Board, pk=board_id)
 
-def board_delete(request):
-    return redirect('board_detail')
+    if request.method == 'POST':
+        comment = BoardComment()
+        comment.content = request.POST['comment']
+        comment.title = board
+        if request.user.is_authenticated:
+            comment.author = request.user
+        comment.pub_date = timezone.now()
+        comment.save()
+    return redirect('board_detail', board_id)
+
+def board_delete(request, board_id, comment_id):
+    comment = get_object_or_404(BoardComment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+    return redirect('board_detail', board_id)
 
 def lost_new(request, stuff_id):
     lost = get_object_or_404(Lost, pk=stuff_id)
