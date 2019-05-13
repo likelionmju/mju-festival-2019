c92b3e4b336a14c716c369bacc4ea330de03c823
Jeonghun <bjh5230@gmail.com>
2019-05-14 01:54:11 +0900

    Modified View : account form validity check

--- account/views.py
+++ account/views.py
@@ -3,30 +3,56 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-# Create your views here.
+# register
 def register(request):
     if request.method == 'POST':
-        if request.POST['pw'] == request.POST['confirm-pw']:
+        username = request.POST['id']
+        password = request.POST['pw']
+        password2 = request.POST['confirm-pw']
+
+        check1 = exist_user_check(username)
+        check2 = password_check(password, password2)
+
+        if check1 and check2:
             user = User.objects.create_user(
-                request.POST['id'], password=request.POST['pw']
+                username, password
             )
             auth.login(request, user)
             return redirect('home')
-        else:
-            return HttpResponse('입력 정보를 확인하세요.')
+        elif not check1:
+            message = "이미 존재하는 아이디입니다"
+            return render(request, 'register.html', {'message':message})
+        elif not check2: 
+            message = "비밀번호를 확인하세요"
+            return render(request, 'register.html', {'message':message})
     else:
         return render(request, 'register.html')
+
+def exist_user_check(username):
+    if not User.objects.filter(username=username).exists():
+        return True
+    else: 
+        return False
+
+def password_check(password, password2):
+    if password == password2:
+        return True
+    else:
+        return False
+
 
 def login(request):
     if request.method == 'POST':
         username = request.POST['id']
         password = request.POST['pw']
+
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
             return redirect ('home')
         else:
-            return HttpResponse('입력 정보를 확인하세요.')
+            message="로그인 정보를 확인하세요"
+            return render(request, 'login.html', {'message':message})
     else:
         return render(request, 'login.html')
 
